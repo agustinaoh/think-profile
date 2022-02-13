0751389d01ba57d4dfdc389e1fc892740816794f
agustina ohan <agustina.oh@gmail.com>
2022-02-13 21:08:07 +0100

    updated avatar images

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -5,9 +5,28 @@ import Image3 from "../img/avatars/3.jpg";
 import Image4 from "../img/avatars/4.jpg";
 import Image5 from "../img/avatars/5.jpg";
 import Image6 from "../img/avatars/6.jpg";
+import Image7 from "../img/avatars/7.jpg";
+import Image8 from "../img/avatars/8.jpg";
+import Image9 from "../img/avatars/9.jpg";
+import Image10 from "../img/avatars/10.jpg";
+import Image11 from "../img/avatars/11.jpg";
+import Image12 from "../img/avatars/12.jpg";
 
 export const Avatar = () => {
-  const imagesArray = [Image1, Image2, Image3, Image4, Image5, Image6];
+  const imagesArray = [
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+    Image5,
+    Image6,
+    Image7,
+    Image8,
+    Image9,
+    Image10,
+    Image11,
+    Image12,
+  ];
   const randomImage =
     imagesArray[Math.floor(Math.random() * imagesArray.length)];
 
